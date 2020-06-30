127ef7cd37165edde87c69eb1e259861de3f7b41
Asep Cahyana <asep.ce3@gmail.com>
2020-07-01 06:58:49 +0700

    Injecting objects

--- src/org/cahyana/demo/Triangle.java
+++ src/org/cahyana/demo/Triangle.java
@@ -1,28 +1,38 @@
 package org.cahyana.demo;
 
 public class Triangle {
-	
-	private String type;
-	private int hight;
-	
-	public Triangle(String type, int hight) {
-		this.type = type;
-		this.hight = hight;
-	}
-	
-	public int getHight() {
-		return hight;
-	}
-	
-	public String getType() {
-		return type;
+
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
+
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setType(String type) {
-		this.type = type;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public void draw() {
-		System.out.println(getType() + " Triangle drawing of height " + getHight());
+		System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
+		System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
+		System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
 	}
 }
