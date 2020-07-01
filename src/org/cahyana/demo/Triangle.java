4300714a736b06361f48cf6a70b01e0978523984
Asep Cahyana <asep.ce3@gmail.com>
2020-07-01 13:40:44 +0700

    bean autowiring

--- src/org/cahyana/demo/Triangle.java
+++ src/org/cahyana/demo/Triangle.java
@@ -4,19 +4,37 @@ import java.util.List;
 
 public class Triangle {
 
-	private List<Point> points;
+	private Point pointA;
+	private Point pointB;
+	private Point pointC;
 
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
+	}
+
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
 	}
 
 	public void draw() {
-		for (Point p : points) {
-			System.out.println("Point = (" + p.getX() + ", " + p.getY() + ")");
-		}
+		System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
+		System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
+		System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
 	}
 }
