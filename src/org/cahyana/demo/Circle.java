ab6511c33ab8bb73fef5705207084094a8d38aed
Asep Cahyana <asep.ce3@gmail.com>
2020-07-01 19:19:49 +0700

    Autowired annotation

--- src/org/cahyana/demo/Circle.java
+++ src/org/cahyana/demo/Circle.java
@@ -1,6 +1,7 @@
 package org.cahyana.demo;
 
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Circle implements Shape {
 
@@ -10,7 +11,8 @@ public class Circle implements Shape {
 		return center;
 	}
 
-	@Required
+	@Autowired
+	@Qualifier("tengah")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
